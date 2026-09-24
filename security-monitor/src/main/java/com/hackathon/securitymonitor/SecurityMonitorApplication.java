f2115380a61de612aed6185c3e4351a055ef283a
Supriya_2007 <supriyamurugan0202@gmail.com>
2026-09-24 05:54:30 +0000

    Initial Spring Boot project setup

---
+++ security-monitor/src/main/java/com/hackathon/securitymonitor/SecurityMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon.securitymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityMonitorApplication.class, args);
+    }
+}
